983f6a1180932ee0e442027285f37ab46abb2c7b
l <l>
2021-11-16 01:59:59 +0500

    init

---
+++ ExamRetake/FileWorker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamRetake
+{
+	public class FileWorker
+	{
+		private string GetFilePath(string fileName)
+		{
+			string filePath = Environment.CurrentDirectory;
+			filePath = filePath.Substring(0, filePath.IndexOf("bin")) + fileName;
+			return filePath;
+		}
+
+		public string[] ReadFile(string fileName)
+		{
+			string filePath = GetFilePath(fileName);
+			string[] stringData = File.ReadAllLines(filePath);
+
+			return stringData;
+		}
+
+		public void WriteInFile(string fileName, string[] dataArray)
+		{
+			dataArray.ToList().ForEach(s => WriteInFile(fileName, s));
+		}
+
+		public void WriteInFile(string fileName, string data)
+		{
+			string filePath = GetFilePath(fileName);
+
+			using (StreamWriter sw = new(filePath))
+			{
+				sw.WriteLine(data);
+			}
+		}
+	}
+}
---
+++ ExamRetake/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExamRetake
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
